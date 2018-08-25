8cdb6053768b29e2b81cbf2c1538c9faa8c1b5cd
Julio Marins <aj07mm@hotmail.com>
2018-08-25 14:35:02 -0300

    not working with related manager (#1)

--- testmanager/core/models.py
+++ testmanager/core/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.db.models.query import QuerySet
 
 
+class Country(models.Model):
+    name = models.CharField(max_length=10, blank=True, null=True)
+
+
 class PersonQuerySet(QuerySet):
     def set(self, slug, **kwargs):
         return self.filter(slug=slug).update(**kwargs)
@@ -10,5 +14,8 @@ class PersonQuerySet(QuerySet):
 class Person(models.Model):
     name = models.CharField(max_length=100, null=True)
     slug = models.CharField(max_length=10, null=True)
+    country = models.ForeignKey(Country, blank=True, null=True, related_name='persons')
 
     objects = PersonQuerySet.as_manager()
+
+
--- testmanager/core/tests/models/test_person_query_set.py
+++ testmanager/core/tests/models/test_person_query_set.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from core.models import Person
+from core.models import Country, Person
 
 class TestSet(TestCase):
     def test_just_update_records_with_the_same_slug(self):
@@ -8,6 +8,20 @@ class TestSet(TestCase):
         Person.objects.create(slug='bruce', name='Ill be back')
 
         Person.objects.set('batman', name='###')
+
+        expected_value = 2
+        result = Person.objects.filter(name='###').count()
+
+        self.assertEqual(result, expected_value)
+
+
+    def test_just_update_records_with_the_same_slug_using_related_manager(self):
+        Person.objects.create(slug='batman', name='John')
+        Person.objects.create(slug='batman', name='Connor')
+        Person.objects.create(slug='bruce', name='Ill be back')
+
+        country= Country.objects.create(name='Ireland')
+        country.persons.set('batman', name='####')
 
         expected_value = 2
         result = Person.objects.filter(name='###').count()
