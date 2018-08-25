ff2e38829eeb651e44fcf333f57778f294663dad
Luis Vilella <luivilella@gmail.com>
2018-08-25 15:32:18 -0300

    feat(core models): using related manager
    - trying to understand why set method not work when used as related manager

--- testmanager/core/models.py
+++ testmanager/core/models.py
@@ -10,12 +10,16 @@ class PersonQuerySet(QuerySet):
     def set(self, slug, **kwargs):
         return self.filter(slug=slug).update(**kwargs)
 
+    def set2(self, slug, **kwargs):
+        return self.set(slug, **kwargs)
+
 
 class Person(models.Model):
     name = models.CharField(max_length=100, null=True)
     slug = models.CharField(max_length=10, null=True)
     country = models.ForeignKey(
-        Country, blank=True, null=True, related_name='persons'
+        Country, blank=True, null=True, related_name='persons',
+        on_delete=models.SET_NULL
     )
 
     objects = PersonQuerySet.as_manager()
--- testmanager/core/tests/models/test_person_query_set.py
+++ testmanager/core/tests/models/test_person_query_set.py
@@ -16,12 +16,15 @@ class TestSet(TestCase):
 
 
     def test_just_update_records_with_the_same_slug_using_related_manager(self):
-        Person.objects.create(slug='batman', name='John')
-        Person.objects.create(slug='batman', name='Connor')
-        Person.objects.create(slug='bruce', name='Ill be back')
+        country = Country.objects.create(name='Ireland')
 
-        country= Country.objects.create(name='Ireland')
-        country.persons.set('batman', name='####')
+        Person.objects.create(slug='batman', name='John', country=country)
+        Person.objects.create(slug='batman', name='Connor', country=country)
+        Person.objects.create(
+            slug='bruce', name='Ill be back', country=country
+        )
+
+        country.persons.set('batman', name='###')
 
         expected_value = 2
         result = Person.objects.filter(name='###').count()
